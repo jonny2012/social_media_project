42ea924fe59c1224e91493f45b3250699aa5d838
Jonny <vanea.ss21@gmail.com>
2024-11-22 15:41:01 +0100

    error page created

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,51 +1,58 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import SideMenu from "../modules/sideMenu/SideMenu";
 
 function ErrorPage() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        flexDirection: "column",
-        textAlign: "center",
-        bgcolor: "#f8f9fa",
-        padding: 3,
-      }}
-    >
-      <Box
+    <>
+      <SideMenu />
+      <Container
         sx={{
           display: "flex",
           justifyContent: "center",
+
           alignItems: "center",
-          mb: 3,
+          minHeight: "100vh",
+          flexDirection: "row",
+          textAlign: "center",
+          bgcolor: "#f8f9fa",
+          padding: 3,
         }}
       >
-        <img
-          src="./../assets//Background.jpg"
-          alt="Instagram Mockup"
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-            width: "300px",
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mb: 3,
           }}
-        />
-      </Box>
+        >
+          <Box
+            sx={{
+              width: "100%",
+              height: "40vh",
+              backgroundImage: `url('./../assets/image.png')`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></Box>
+        </Box>
 
-      {/* 404 Error Text */}
-      <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }}>
-        Oops! Page Not Found (404 Error)
-      </Typography>
-      <Typography
-        variant="body1"
-        sx={{ color: "text.secondary", maxWidth: 600 }}
-      >
-        We're sorry, but the page you're looking for doesn't seem to exist. If
-        you typed the URL manually, please double-check the spelling. If you
-        clicked on a link, it may be outdated or broken.
-      </Typography>
-    </Box>
+        <Box>
+          <Typography variant="h4" sx={{ fontWeight: "bold", mb: 1 }}>
+            Oops! Page Not Found (404 Error)
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ color: "text.secondary", maxWidth: 600 }}
+          >
+            We `re sorry, but the page you`re looking for doesn`t seem to exist.
+            If you typed the URL manually, please double-check the spelling. If
+            you clicked on a link, it may be outdated or broken.
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
 
